027a77f0f20fe5dc4e60d0d4bf2e1f21499c73fc
Giovanny Occident <GioOccident@gmail.com>
2023-04-13 15:30:09 -0400

    update

--- db/connections.js
+++ db/connections.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 
-// mongoose.Promise = Promise
-
 const URL = process.env.PROD_MONGODB || "mongodb://127.0.0.1:27017/backend_api";
 
 mongoose.set("returnOriginal", false)
